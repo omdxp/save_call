9d26f597ec57be73c1c955f39a2a83e9dc6136b1
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-02-22 18:48:42 +0100

    draw basic home ui 🧱

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:save_call/utils/constants.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -13,11 +14,134 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         title: Text('Home'),
       ),
-      body: Container(
-        child: Center(
-          child: Text('Hello, World!'),
+      drawer: Drawer(
+        elevation: 8.5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              'Profile',
+              style: TextStyle(fontSize: FONT_SIZE_TEXT),
+            ),
+            SizedBox(
+              height: SPACE_HEIGHT,
+            ),
+            Text(
+              'Calls',
+              style: TextStyle(fontSize: FONT_SIZE_TEXT),
+            ),
+            SizedBox(
+              height: SPACE_HEIGHT,
+            ),
+            Text(
+              'Parameters',
+              style: TextStyle(fontSize: FONT_SIZE_TEXT),
+            ),
+          ],
         ),
       ),
+      body: Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'User input',
+                style: TextStyle(fontSize: 25, color: Colors.red[600]),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.filter_9),
+                          onPressed: () => {},
+                        ),
+                        SizedBox(
+                          width: SPACE_WIDTH,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.filter_8),
+                          onPressed: () => {},
+                        ),
+                        SizedBox(
+                          width: SPACE_WIDTH,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.filter_7),
+                          onPressed: () => {},
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: SPACE_HEIGHT,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.filter_6),
+                          onPressed: () => {},
+                        ),
+                        SizedBox(
+                          width: SPACE_WIDTH,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.filter_5),
+                          onPressed: () => {},
+                        ),
+                        SizedBox(
+                          width: SPACE_WIDTH,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.filter_4),
+                          onPressed: () => {},
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: SPACE_HEIGHT,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.filter_3),
+                          onPressed: () => {},
+                        ),
+                        SizedBox(
+                          width: SPACE_WIDTH,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.filter_2),
+                          onPressed: () => {},
+                        ),
+                        SizedBox(
+                          width: SPACE_WIDTH,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.filter_1),
+                          onPressed: () => {},
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: SPACE_HEIGHT,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.filter),
+                      onPressed: () => {},
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
     );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+// font size constants
+const double FONT_SIZE_TEXT = 25;
+const double FONT_SIZE_TITLE = 75;
+const double FONT_SIZE_SUBTITLE = 45;
+
+// spaces constants
+const double SPACE_HEIGHT = 8;
+const double SPACE_WIDTH = 8;
